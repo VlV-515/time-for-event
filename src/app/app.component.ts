79edb15b2ff89c081d7d8e18111f7f58c06c4630
VlV <92279938+VlV-515@users.noreply.github.com>
2022-03-01 18:41:21 -0600

    feat⚙️:Add catching dataa from from

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+export interface EventInterface {
+  name: string;
+  date: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'time-for-event';
+  onSave(data: EventInterface): void {
+    console.log(data);
+    
+  }
 }
